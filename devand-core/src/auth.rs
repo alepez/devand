85f4fe4518259516b6a88e622ce97a2119c94c85
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-06-12 08:56:59 +0200

    Fix tests

--- devand-core/src/auth.rs
+++ devand-core/src/auth.rs
@@ -32,27 +32,27 @@ mod test {
 
     #[test]
     fn invalid_password_len() {
-        assert!(is_valid_password("234567a"));
+        assert!(!is_valid_password("234567a"));
     }
 
     #[test]
     fn invalid_password_only_num() {
-        assert!(is_valid_password("1234567890"));
+        assert!(!is_valid_password("1234567890"));
     }
 
     #[test]
     fn invalid_password_only_alpha() {
-        assert!(is_valid_password("qwertyuiop"));
+        assert!(!is_valid_password("qwertyuiop"));
     }
 
     #[test]
     fn valid_username_len() {
-        assert!(is_valid_password("ap1"));
+        assert!(is_valid_username("ap1"));
     }
 
     #[test]
     fn invalid_username_wrong_character() {
-        assert!(!is_valid_password("alepez%"));
-        assert!(!is_valid_password("AlePez"));
+        assert!(!is_valid_username("alepez%"));
+        assert!(!is_valid_username("AlePez"));
     }
 }
