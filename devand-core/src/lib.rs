28863e22c4bb0f3e24f081d16d80a357da06b4ed
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-06-15 21:37:04 +0200

    Refactoring

--- devand-core/src/lib.rs
+++ devand-core/src/lib.rs
@@ -1,13 +1,14 @@
 mod affinity;
+mod schedule;
 pub mod auth;
 pub mod mock;
 
 use serde::{Deserialize, Serialize};
 use std::cmp::Ord;
 use std::collections::BTreeMap;
-use std::convert::TryFrom;
 use strum_macros::{Display, EnumIter, EnumString};
 
+pub use schedule::{DaySchedule, Schedule, WeekSchedule};
 pub use affinity::{Affinity, AffinityParams};
 
 pub type UserId = i32;
@@ -69,86 +70,6 @@ pub struct UserSettings {
     pub schedule: Schedule,
     /// User can disable all activities without losing schedule
     pub vacation_mode: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub enum Schedule {
-    /// Disabled
-    Never,
-    /// Schedule every week in the future
-    Weekly(WeekSchedule),
-}
-
-impl Default for Schedule {
-    fn default() -> Self {
-        Schedule::Never
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Default, Ord, PartialOrd, Eq, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub struct DaySchedule {
-    pub hours: [bool; 24],
-}
-
-impl TryFrom<&str> for DaySchedule {
-    type Error = ();
-
-    fn try_from(s: &str) -> Result<Self, ()> {
-        if s == "*" {
-            return Ok(Self::always());
-        }
-
-        let numbers: Vec<usize> = s.split(',').filter_map(|x| x.parse().ok()).collect();
-
-        if numbers.is_empty() {
-            return Ok(Self::never());
-        }
-
-        let mut hours: [bool; Self::HOURS_IN_DAY] = [false; Self::HOURS_IN_DAY];
-
-        for n in numbers {
-            if n < hours.len() {
-                hours[n] = true;
-            }
-        }
-
-        Ok(DaySchedule { hours })
-    }
-}
-
-impl DaySchedule {
-    pub const HOURS_IN_DAY: usize = 24;
-
-    pub fn never() -> Self {
-        DaySchedule {
-            hours: [false; Self::HOURS_IN_DAY],
-        }
-    }
-
-    pub fn always() -> Self {
-        DaySchedule {
-            hours: [true; Self::HOURS_IN_DAY],
-        }
-    }
-
-    pub fn new(hours: [bool; Self::HOURS_IN_DAY]) -> Self {
-        DaySchedule { hours }
-    }
-}
-
-/// Week scheduling
-#[derive(Debug, Serialize, Deserialize, Clone, Default, Ord, PartialOrd, Eq, PartialEq)]
-#[serde(rename_all = "snake_case")]
-pub struct WeekSchedule {
-    pub mon: DaySchedule,
-    pub tue: DaySchedule,
-    pub wed: DaySchedule,
-    pub thu: DaySchedule,
-    pub fri: DaySchedule,
-    pub sat: DaySchedule,
-    pub sun: DaySchedule,
 }
 
 #[derive(
@@ -371,18 +292,6 @@ mod tests {
     fn user_example() {
         let user = mock::user();
         let _json = serde_json::to_string(&user).unwrap();
-    }
-
-    #[test]
-    fn hourly_schedule_from_comma_separated_list() {
-        let s = "5,7,21";
-        let schedule = DaySchedule::try_from(s).unwrap();
-        assert!(schedule.hours[4] == false);
-        assert!(schedule.hours[5] == true);
-        assert!(schedule.hours[6] == false);
-        assert!(schedule.hours[7] == true);
-        assert!(schedule.hours[21] == true);
-        assert!(schedule.hours[22] == false);
     }
 
     #[test]
---
+++ devand-core/src/schedule.rs
@@ -0,0 +1,100 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::Ord;
+use std::convert::TryFrom;
+
+#[derive(Debug, Serialize, Deserialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum Schedule {
+    /// Disabled
+    Never,
+    /// Schedule every week in the future
+    Weekly(WeekSchedule),
+}
+
+impl Default for Schedule {
+    fn default() -> Self {
+        Schedule::Never
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default, Ord, PartialOrd, Eq, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct DaySchedule {
+    pub hours: [bool; 24],
+}
+
+impl TryFrom<&str> for DaySchedule {
+    type Error = ();
+
+    fn try_from(s: &str) -> Result<Self, ()> {
+        if s == "*" {
+            return Ok(Self::always());
+        }
+
+        let numbers: Vec<usize> = s.split(',').filter_map(|x| x.parse().ok()).collect();
+
+        if numbers.is_empty() {
+            return Ok(Self::never());
+        }
+
+        let mut hours: [bool; Self::HOURS_IN_DAY] = [false; Self::HOURS_IN_DAY];
+
+        for n in numbers {
+            if n < hours.len() {
+                hours[n] = true;
+            }
+        }
+
+        Ok(DaySchedule { hours })
+    }
+}
+
+impl DaySchedule {
+    pub const HOURS_IN_DAY: usize = 24;
+
+    pub fn never() -> Self {
+        DaySchedule {
+            hours: [false; Self::HOURS_IN_DAY],
+        }
+    }
+
+    pub fn always() -> Self {
+        DaySchedule {
+            hours: [true; Self::HOURS_IN_DAY],
+        }
+    }
+
+    pub fn new(hours: [bool; Self::HOURS_IN_DAY]) -> Self {
+        DaySchedule { hours }
+    }
+}
+
+/// Week scheduling
+#[derive(Debug, Serialize, Deserialize, Clone, Default, Ord, PartialOrd, Eq, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct WeekSchedule {
+    pub mon: DaySchedule,
+    pub tue: DaySchedule,
+    pub wed: DaySchedule,
+    pub thu: DaySchedule,
+    pub fri: DaySchedule,
+    pub sat: DaySchedule,
+    pub sun: DaySchedule,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hourly_schedule_from_comma_separated_list() {
+        let s = "5,7,21";
+        let schedule = DaySchedule::try_from(s).unwrap();
+        assert!(schedule.hours[4] == false);
+        assert!(schedule.hours[5] == true);
+        assert!(schedule.hours[6] == false);
+        assert!(schedule.hours[7] == true);
+        assert!(schedule.hours[21] == true);
+        assert!(schedule.hours[22] == false);
+    }
+}
