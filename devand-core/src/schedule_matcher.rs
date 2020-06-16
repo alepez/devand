1c8c257d71ca0a304c4dc5cc671dc26e80f1c4f1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-06-16 10:58:16 +0200

    Add ScheduleMatrix

--- devand-core/src/schedule_matcher.rs
+++ devand-core/src/schedule_matcher.rs
@@ -18,12 +18,10 @@ impl std::ops::Index<(UserId, UserId)> for AffinityMatrix {
 /// The resulting matrix size is ((n * (n-1)) / 2), where n is the max
 /// UserId.
 impl From<Vec<(UserId, AffinityParams)>> for AffinityMatrix {
-    fn from(mut ua: Vec<(UserId, AffinityParams)>) -> Self {
-        ua.sort_unstable_by_key(|x| x.0);
+    fn from(ua: Vec<(UserId, AffinityParams)>) -> Self {
+        if let Some((UserId(max_user_id), _)) = ua.iter().max_by_key(|x| x.0) {
+            let size = (((max_user_id + 1) * max_user_id) / 2) as usize;
 
-        if let Some((UserId(last), _)) = ua.last() {
-            let max_user_id = (1 + last) as usize;
-            let size = (max_user_id * (max_user_id - 1)) / 2;
             let mut data = Vec::with_capacity(size);
             data.resize_with(size, Default::default);
 
@@ -43,12 +41,50 @@ impl From<Vec<(UserId, AffinityParams)>> for AffinityMatrix {
     }
 }
 
+struct Hour(i32);
+
+#[derive(Debug)]
+struct ScheduleMatrix(Vec<bool>);
+
+impl std::ops::Index<(UserId, Hour)> for ScheduleMatrix {
+    type Output = bool;
+
+    fn index(&self, pair: (UserId, Hour)) -> &Self::Output {
+        let (UserId(i), Hour(h)) = pair;
+        let p = (i as usize) * DaySchedule::HOURS_IN_DAY + (h as usize);
+        &self.0[p]
+    }
+}
+
+impl From<Vec<(UserId, DaySchedule)>> for ScheduleMatrix {
+    fn from(us: Vec<(UserId, DaySchedule)>) -> Self {
+        if let Some((UserId(max_user_id), _)) = us.iter().max_by_key(|x| x.0) {
+            let size = (1 + (*max_user_id as usize)) * DaySchedule::HOURS_IN_DAY;
+
+            let mut data = Vec::with_capacity(size);
+            data.resize_with(size, Default::default);
+
+            for h in 0..24 {
+                for (UserId(i), day) in us.iter() {
+                    let in_schedule = day.hours[h];
+                    let p = (*i as usize) * DaySchedule::HOURS_IN_DAY + h;
+                    let cell = unsafe { data.get_unchecked_mut(p) };
+                    *cell = in_schedule;
+                }
+            }
+            Self(data)
+        } else {
+            Self(Vec::default())
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{Language, LanguagePreference, Level, Priority};
-
     use super::*;
+    use crate::{Language, LanguagePreference, Level, Priority};
+    use std::convert::TryFrom;
+
     #[test]
     fn calculate_affinity_matrix() {
         let params = vec![
@@ -82,22 +118,22 @@ mod tests {
                 ]),
             ),
             (
-                UserId(2),
-                AffinityParams::from(vec![(
-                    Language::Rust,
-                    LanguagePreference {
-                        level: Level::Novice,
-                        priority: Priority::High,
-                    },
-                )]),
-            ),
-            (
                 UserId(3),
                 AffinityParams::from(vec![(
                     Language::CPlusPlus,
                     LanguagePreference {
                         level: Level::Novice,
                         priority: Priority::No,
+                    },
+                )]),
+            ),
+            (
+                UserId(2),
+                AffinityParams::from(vec![(
+                    Language::Rust,
+                    LanguagePreference {
+                        level: Level::Novice,
+                        priority: Priority::High,
                     },
                 )]),
             ),
@@ -110,5 +146,29 @@ mod tests {
         assert!(matrix[(UserId(0), UserId(2))] == Some(Affinity::NONE));
         assert!(matrix[(UserId(0), UserId(3))] == Some(Affinity::NONE));
         assert!(matrix[(UserId(1), UserId(2))] == Some(Affinity::FULL));
+    }
+
+    #[test]
+    fn calculate_schedule_matrix() {
+        let params = vec![
+            (UserId(0), DaySchedule::try_from("1,2").unwrap()),
+            (UserId(1), DaySchedule::try_from("1,2,3").unwrap()),
+            (UserId(2), DaySchedule::try_from("2,3,5,6").unwrap()),
+            (UserId(3), DaySchedule::try_from("3,4,5").unwrap()),
+        ];
+
+        let expected_len = params.len() * 24;
+        let matrix = ScheduleMatrix::from(params);
+
+        assert!(matrix.0.len() == expected_len);
+        assert!(matrix[(UserId(0), Hour(1))] == true);
+        assert!(matrix[(UserId(2), Hour(3))] == true);
+        assert!(matrix[(UserId(2), Hour(5))] == true);
+        assert!(matrix[(UserId(2), Hour(1))] == false);
+        assert!(matrix[(UserId(1), Hour(5))] == false);
+        assert!(matrix[(UserId(3), Hour(6))] == false);
+        // assert!(matrix[(UserId(0), Hour(1))] == vec![UserId(1)]);
+        // assert!(matrix[(UserId(2), Hour(3))] == vec![UserId(1), UserId(3)]);
+        // assert!(matrix[(UserId(2), Hour(5))] == vec![UserId(3)]);
     }
 }
