0ed55ac7551b7af2cf57184e382645e3ed9ef728
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-06-17 10:22:26 +0200

    Avoid loading css twice in dashboard

--- devand-ui/bootstrap-prod.js
+++ devand-ui/bootstrap-prod.js
@@ -1,5 +1,4 @@
-// In production, do not imprt style.scss, as it is provided by devand-web
-// import './static/style.scss';
+import './static/style.scss';
 
 import("./pkg").then(module => {
   module.run_app();
