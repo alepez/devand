ed42999572861819326bffa10e50ccf10e44f496
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-07-01 15:00:22 +0200

    Refactoring SignedToken

--- devand-crypto/src/email_verification_token.rs
+++ devand-crypto/src/email_verification_token.rs
@@ -9,32 +9,3 @@ pub struct EmailVerification {
 impl signed_token::Signable for EmailVerification {
     const EXP_SECONDS: i64 = 3 * 60 * 60;
 }
-
-#[derive(Clone)]
-pub struct EmailVerificationToken(signed_token::SignedToken);
-
-// TODO Use traits/derive/macro to automatically implement this
-impl EmailVerificationToken {
-    pub fn new(data: &EmailVerification, encoder: &signed_token::Encoder) -> Self {
-        let token = encoder.encode(data).expect("Token is encoded");
-        Self(token)
-    }
-
-    pub fn decode(&self, decoder: &signed_token::Decoder) -> Option<EmailVerification> {
-        decoder.decode(&self.0)
-    }
-}
-
-// TODO Use traits/derive/macro to automatically implement this
-impl Into<String> for EmailVerificationToken {
-    fn into(self) -> String {
-        self.0.into()
-    }
-}
-
-// TODO Use traits/derive/macro to automatically implement this
-impl From<String> for EmailVerificationToken {
-    fn from(s: String) -> Self {
-        EmailVerificationToken(signed_token::SignedToken::from(s))
-    }
-}
