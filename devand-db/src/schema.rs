48b1cd28b853a0d0c372ede2814fb646ced44841
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-07-14 00:16:41 +0200

    Reformat code

--- devand-db/src/schema.rs
+++ devand-db/src/schema.rs
@@ -34,9 +34,4 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(
-    auth,
-    chats,
-    messages,
-    users,
-);
+allow_tables_to_appear_in_same_query!(auth, chats, messages, users,);
