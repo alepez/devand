dcb867b99efcb0b8e93746c14f7fcd58360d13bb
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-07-17 23:39:22 +0200

    Use rng for affinities mock

--- devand-ui/src/app/services/affinities_service/mock.rs
+++ devand-ui/src/app/services/affinities_service/mock.rs
@@ -2,75 +2,67 @@ use super::FetchCallback;
 use devand_core::Affinity;
 use devand_core::UserAffinity;
 
+use fake::faker::lorem::en::*;
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use fake::Fake;
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+
 pub struct AffinitiesService {
     callback: FetchCallback,
+    rng: StdRng,
 }
 
 impl AffinitiesService {
     pub fn new(callback: FetchCallback) -> Self {
-        Self { callback }
+        let rng = StdRng::seed_from_u64(42);
+        Self { callback, rng }
     }
 
     pub fn restore(&mut self) {
-        self.callback.emit(Ok(mock_affinities()))
+        self.callback.emit(Ok(fake_affinities(&mut self.rng)))
     }
 }
 
-fn mock_affinities() -> Vec<UserAffinity> {
-    vec![
-        UserAffinity::new(mock_user("Albert Einstein"), Affinity::from_number(0.1)),
-        UserAffinity::new(mock_user("Isaac Newton"), Affinity::from_number(0.2)),
-        UserAffinity::new(mock_user("James Clerk Maxwell"), Affinity::from_number(0.5)),
-        UserAffinity::new(mock_user("Max Plank"), Affinity::from_number(1.0)),
-    ]
+fn fake_affinities(rng: &mut StdRng) -> Vec<UserAffinity> {
+    let mut users = Vec::new();
+    let n = 6;
+
+    for _ in 0..n {
+        let aff_num: f64 = rng.gen_range(0.0, 1.0);
+        users.push(UserAffinity::new(
+            fake_user(rng),
+            Affinity::from_number(aff_num),
+        ));
+    }
+
+    users
 }
 
-fn mock_user(name: &str) -> devand_core::PublicUserProfile {
+fn fake_user(rng: &mut StdRng) -> devand_core::PublicUserProfile {
     use devand_core::*;
-    use std::collections::BTreeMap;
+    use rand::seq::IteratorRandom;
+    use strum::IntoEnumIterator;
 
-    let mut languages = BTreeMap::default();
+    let name: String = Name(EN).fake_with_rng(rng);
+    let user_id: i32 = rng.gen_range(1, 1_000_000_000);
 
-    languages.insert(
-        Language::C,
-        LanguagePreference {
-            level: Level::Expert,
-            priority: Priority::Low,
-        },
-    );
-    languages.insert(
-        Language::JavaScript,
-        LanguagePreference {
-            level: Level::Proficient,
-            priority: Priority::Low,
-        },
-    );
-    languages.insert(
-        Language::CPlusPlus,
-        LanguagePreference {
-            level: Level::Expert,
-            priority: Priority::Low,
-        },
-    );
-    languages.insert(
-        Language::Rust,
-        LanguagePreference {
-            level: Level::Proficient,
-            priority: Priority::High,
-        },
-    );
-    languages.insert(
-        Language::Go,
-        LanguagePreference {
-            level: Level::Novice,
-            priority: Priority::No,
-        },
-    );
+    let mut languages = std::collections::BTreeMap::default();
+
+    for lang in Language::iter() {
+        if rng.gen_bool(0.2) {
+            let level = Level::iter().choose(rng).unwrap();
+            let priority = Priority::iter().choose(rng).unwrap();
+            languages.insert(lang, LanguagePreference { level, priority });
+        }
+    }
 
     let languages = Languages(languages);
 
     PublicUserProfile {
-        id: UserId(3),
+        id: UserId(user_id),
         username: name
             .to_string()
             .to_lowercase()
