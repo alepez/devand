ab9c4ecd8dfb2b92613aa4212eab381d1b626f69
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-12 16:56:39 +0200

    Add tests for spoken languages

--- devand-core/src/spoken_languages.rs
+++ devand-core/src/spoken_languages.rs
@@ -21,3 +21,22 @@ pub enum SpokenLanguage {
     English,
     Italian,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn spoken_lang_serde() {
+        let x = vec![SpokenLanguage::English, SpokenLanguage::Italian];
+        let j = serde_json::to_string(&x).unwrap();
+        assert_eq!("[\"english\",\"italian\"]", j);
+    }
+
+    #[test]
+    fn spoken_lang_display() {
+        let x = SpokenLanguage::Italian;
+        let f = format!("{}", x);
+        assert_eq!("Italian", f);
+    }
+}
