7149f14927083da2142752f8b51f17cbc709cc7e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-12 17:23:30 +0200

    Add iso languages support

--- devand-core/src/spoken_languages.rs
+++ devand-core/src/spoken_languages.rs
@@ -1,25 +1,54 @@
+use isolang::Language as IsoLang;
 use serde::{Deserialize, Serialize};
 use std::cmp::Ord;
-use strum_macros::{Display, EnumIter, EnumString};
+use strum_macros::{EnumIter, EnumString};
 
 #[derive(
-    Debug,
-    Serialize,
-    Deserialize,
-    Ord,
-    PartialOrd,
-    Eq,
-    PartialEq,
-    Copy,
-    Clone,
-    EnumIter,
-    Display,
-    EnumString,
+    Debug, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, EnumIter, EnumString,
 )]
 #[serde(rename_all = "snake_case")]
 pub enum SpokenLanguage {
-    English,
+    Ara,
+    Ben,
+    Cmn,
+    Deu,
+    Fra,
+    Hin,
+    Jpn,
+    Pan,
+    Por,
+    Rus,
+    Spa,
+    // TODO Change to Ita, only for back compatibility
     Italian,
+    // TODO Change to Eng, only for back compatibility
+    English,
+}
+
+impl SpokenLanguage {
+    pub fn iso(&self) -> IsoLang {
+        match self {
+            SpokenLanguage::Ara => IsoLang::Ara,
+            SpokenLanguage::Ben => IsoLang::Ben,
+            SpokenLanguage::Cmn => IsoLang::Cmn,
+            SpokenLanguage::Deu => IsoLang::Deu,
+            SpokenLanguage::English => IsoLang::Eng,
+            SpokenLanguage::Fra => IsoLang::Fra,
+            SpokenLanguage::Hin => IsoLang::Hin,
+            SpokenLanguage::Italian => IsoLang::Ita,
+            SpokenLanguage::Jpn => IsoLang::Jpn,
+            SpokenLanguage::Pan => IsoLang::Pan,
+            SpokenLanguage::Por => IsoLang::Por,
+            SpokenLanguage::Rus => IsoLang::Rus,
+            SpokenLanguage::Spa => IsoLang::Spa,
+        }
+    }
+}
+
+impl std::fmt::Display for SpokenLanguage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.iso())
+    }
 }
 
 #[cfg(test)]
@@ -30,7 +59,7 @@ mod tests {
     fn spoken_lang_serde() {
         let x = vec![SpokenLanguage::English, SpokenLanguage::Italian];
         let j = serde_json::to_string(&x).unwrap();
-        assert_eq!("[\"english\",\"italian\"]", j);
+        assert_eq!("[\"eng\",\"ita\"]", j);
     }
 
     #[test]
