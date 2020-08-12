4bac5e48cf407de1bcb7d95402c61baa00f5f422
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-12 16:17:31 +0200

    Refactoring

--- devand-db/src/lib.rs
+++ devand-db/src/lib.rs
@@ -19,13 +19,14 @@ extern crate diesel_migrations;
 
 embed_migrations!();
 
-pub fn establish_connection() -> PgConnection {
+fn database_url() -> String {
     dotenv().ok();
+    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+}
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+pub fn establish_connection() -> PgConnection {
+    let url = database_url();
+    PgConnection::establish(&url).unwrap_or_else(|_| panic!("Error connecting to {}", &url))
 }
 
 #[derive(Debug)]
