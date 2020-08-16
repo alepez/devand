alepez/devand

commit d40b461ed8371be670ece6d4b309c3ce71323f59

replace text with textarea for bio #93 (#97)
* replace text with textarea for bio #93

* fixed tabindex on the login page #98

* fix layout to make textarea resposnive

* replaced div with form tag

devand-ui/src/app/components/settings_page.rs
@@ -120,7 +120,7 @@ impl SettingsPage {
         let settings = &user.settings;
 
         html! {
-            <div class="pure-form pure-form-stacked">
+            <form class="pure-form pure-form-stacked">
                 { self.view_profile_panel(user) }
                 {
                     if user.settings.vacation_mode {
@@ -130,7 +130,7 @@ impl SettingsPage {
                     }
                 }
                 { self.view_languages_panel(&settings.languages) }
-            </div>
+            </form>
         }
     }
 
@@ -177,7 +177,7 @@ impl SettingsPage {
                 </div>
                 <div class="pure-control-group">
                     <label for="bio">{ "Bio:" }</label>
-                    <input type="text" name="bio" id="bio" value=&user.bio oninput=self.link.callback(move |e: InputData| Msg::UpdateBio(e.value)) />
+                    <textarea name="bio" class="pure-input-1-1" id="bio" value=&user.bio oninput=self.link.callback(move |e: InputData| Msg::UpdateBio(e.value)) />
                     <span class="pure-form-message-inline">{ "(max 160 characters)" }</span>
                 </div>
                 <div class="pure-control-group">

devand-web/templates/login.html.hbs
@@ -8,16 +8,16 @@
       <fieldset>
         <div class="pure-control-group">
           <label for="username">Username or email address</label>
-          <input type="text" name="username" id="username" value="" placeholder="Username" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
+          <input type="text" tabindex=1 autofocus="autofocus" name="username" id="username" value="" placeholder="Username" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
         </div>
 
         <div class="pure-control-group">
           <label for="password">Password <small><a href="/password_reset">Forgot password?</a></small></label>
-          <input type="password" name="password" id="password" value="" placeholder="Password" />
+          <input type="password" tabindex=2 name="password" id="password" value="" placeholder="Password" />
         </div>
 
         <div class="pure-controls">
-          <button type="submit" class="pure-button pure-button-primary">Sign In</button>
+          <button tabindex=3 type="submit" class="pure-button pure-button-primary">Sign In</button>
         </div>
       </fieldset>
     </form>

commit 45998c02954cd80e9639016bed6e45f54e2a5cae

Prevent page unload on form submission
Fix regression introduced in d40b461

devand-ui/src/app/components/settings_page.rs
@@ -131,9 +131,10 @@ fn view_vacation_mode_panel() -> Html {
 impl SettingsPage {
     fn view_settings_panel(&self, user: &User) -> Html {
         let settings = &user.settings;
-
+        // Note: do not change div to form, or submission will trigger
+        // a page unload
         html! {
-            <form class="pure-form pure-form-stacked">
+            <div class="pure-form pure-form-stacked">
                 { self.view_profile_panel(user) }
                 {
                     if user.settings.vacation_mode {
@@ -144,7 +145,7 @@ impl SettingsPage {
                 }
                 { self.view_languages_panel(&settings.languages) }
                 { self.view_spoken_languages_panel(&settings.spoken_languages) }
-            </form>
+            </div>
         }
     }
 