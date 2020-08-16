c069b31802c95bdcb674135b8bfd450a0757eb61
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-16 16:23:57 +0200

    Refactoring

--- devand-ui/src/app/workers/main_worker/http.rs
+++ devand-ui/src/app/workers/main_worker/http.rs
@@ -25,39 +25,33 @@ fn api_url_affinities() -> &'static str {
 pub fn request(worker: &mut MainWorker, msg: Request) {
     match msg {
         Request::Init => {
-            let url = api_url_self_user();
-            let req = fetch::Request::get(url).body(Nothing).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::SelfUserFetched);
+            let req = get(api_url_self_user());
+            worker._fetch_task = task(worker, req, Response::SelfUserFetched);
         }
 
         Request::SaveSelfUser(user) => {
-            let url = api_url_self_user();
-            let req = fetch::Request::put(url).body(make_json_body(user)).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::SelfUserFetched);
+            let req = put(api_url_self_user(), json(user));
+            worker._fetch_task = task(worker, req, Response::SelfUserFetched);
         }
 
         Request::VerifyEmail => {
-            let url = api_url_verify_email();
-            let req = fetch::Request::post(url).body(Nothing).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::Done);
+            let req = post(api_url_verify_email(), Nothing);
+            worker._fetch_task = task(worker, req, Response::Done);
         }
 
         Request::LoadCodeNow => {
-            let url = api_url_code_now();
-            let req = fetch::Request::post(url).body(Nothing).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::CodeNowFetched);
+            let req = post(api_url_code_now(), Nothing);
+            worker._fetch_task = task(worker, req, Response::CodeNowFetched);
         }
 
         Request::LoadPublicUserProfileByUsername(username) => {
-            let url = api_url_user(&username);
-            let req = fetch::Request::get(url).body(Nothing).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::PublicUserProfileFetched);
+            let req = get(&api_url_user(&username));
+            worker._fetch_task = task(worker, req, Response::PublicUserProfileFetched);
         }
 
         Request::LoadAffinities => {
-            let url = api_url_affinities();
-            let req = fetch::Request::get(url).body(Nothing).unwrap();
-            worker._fetch_task = make_task(worker, req, Response::AffinitiesFetched);
+            let req = get(api_url_affinities());
+            worker._fetch_task = task(worker, req, Response::AffinitiesFetched);
         }
 
         // Program should never hit this
@@ -65,11 +59,29 @@ pub fn request(worker: &mut MainWorker, msg: Request) {
     }
 }
 
-fn make_json_body<T: serde::ser::Serialize>(data: T) -> Result<String, anyhow::Error> {
+fn get(url: &str) -> fetch::Request<Nothing> {
+    fetch::Request::get(url).body(Nothing).unwrap()
+}
+
+fn post<IN>(url: &str, body: IN) -> fetch::Request<IN>
+where
+    IN: Into<Text>,
+{
+    fetch::Request::post(url).body(body).unwrap()
+}
+
+fn put<IN>(url: &str, body: IN) -> fetch::Request<IN>
+where
+    IN: Into<Text>,
+{
+    fetch::Request::put(url).body(body).unwrap()
+}
+
+fn json<T: serde::ser::Serialize>(data: T) -> Result<String, anyhow::Error> {
     serde_json::to_string(&data).map_err(|e| anyhow::anyhow!(e))
 }
 
-fn make_task<T, IN>(
+fn task<T, IN>(
     worker: &MainWorker,
     req: fetch::Request<IN>,
     ctor: impl Fn(T) -> Response + 'static,
