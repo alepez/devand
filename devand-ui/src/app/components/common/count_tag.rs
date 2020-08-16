557544ad5558df15a89915cb0ba694008c46e1b1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-17 01:22:19 +0200

    Add CountTag pure component

---
+++ devand-ui/src/app/components/common/count_tag.rs
@@ -0,0 +1,21 @@
+use yew::virtual_dom::VNode;
+use yew::{html, Properties};
+use yewtil::{Pure, PureComponent};
+
+/// Alias to make usability better.
+pub type CountTag = Pure<PureCountTag>;
+
+#[derive(PartialEq, Properties, Clone)]
+pub struct PureCountTag {
+    pub count: usize,
+}
+
+impl PureComponent for PureCountTag {
+    fn render(&self) -> VNode {
+        if self.count > 0 {
+            html! { <span class="devand-count-tag">{ self.count }</span> }
+        } else {
+            html! {}
+        }
+    }
+}
