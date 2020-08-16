ab4997fa59fddf8bb37682a944988513616afded
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-16 18:23:32 +0200

    Cleanup

--- devand-ui/src/app/services/mod.rs
+++ devand-ui/src/app/services/mod.rs
@@ -1,7 +1,5 @@
 mod chat_service;
-mod schedule_service;
 mod security_service;
 
 pub use chat_service::{ChatService, ChatServiceContent};
-pub use schedule_service::{ScheduleService, ScheduleServiceContent};
 pub use security_service::{SecurityService, SecurityServiceContent};
