fc711b02e66f038b8d24e24bc843bcfda96ac0a6
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-08-17 15:35:54 +0200

    Use devand-text in AffinitiesPage

--- devand-text/src/lib.rs
+++ devand-text/src/lib.rs
@@ -5,6 +5,9 @@ pub enum Text {
     Schedule,
     Security,
     Messages,
+    AffinitiesTableDescription,
+    NoMatchingUsersFound,
+    ExtendYourLanguageSelection,
 }
 
 impl ToString for Text {
@@ -16,6 +19,9 @@ impl ToString for Text {
             Text::Schedule => "Schedule".into(),
             Text::Security => "Security".into(),
             Text::Messages => "Messages".into(),
+            Text::AffinitiesTableDescription => "In the table below, you can see a list of developers who love the same languages as you. Just click the chat icon to start chatting and organize your next pair-programming session.".into(),
+            Text::NoMatchingUsersFound => "Sorry, no matching users found. You can try to ".into(),
+            Text::ExtendYourLanguageSelection => "extend your language selection.".into(),
         }
     }
 }
