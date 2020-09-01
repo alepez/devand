a85cb454973890f28cdd397c590b8655174a734f
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-09-02 00:14:29 +0200

    Add AffinitiesTable filter by language

--- devand-ui/src/app/components/affinities_table.rs
+++ devand-ui/src/app/components/affinities_table.rs
@@ -1,11 +1,14 @@
 use crate::app::components::user_affinity_bubble;
 use crate::app::components::LanguageTag;
-use devand_core::UserAffinity;
+use devand_core::{Language, UserAffinity};
+use std::str::FromStr;
+use strum::IntoEnumIterator;
 use yew::prelude::*;
 use yewtil::NeqAssign;
 
 pub struct AffinitiesTable {
     props: Props,
+    state: State,
     link: ComponentLink<Self>,
 }
 
@@ -14,18 +17,33 @@ pub struct Props {
     pub affinities: Vec<UserAffinity>,
 }
 
-pub enum Msg {}
+pub enum Msg {
+    FilterByLanguage(String),
+    Nope,
+}
+
+#[derive(Default)]
+struct State {
+    filter_language: Option<Language>,
+}
 
 impl Component for AffinitiesTable {
     type Message = Msg;
     type Properties = Props;
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { props, link }
+        let state = State::default();
+        Self { props, state, link }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        false
+        match msg {
+            Msg::FilterByLanguage(lang) => {
+                let filter_language = Language::from_str(&lang).ok();
+                self.state.filter_language.neq_assign(filter_language)
+            }
+            Msg::Nope => false,
+        }
     }
 
     fn change(&mut self, props: Self::Properties) -> ShouldRender {
@@ -33,16 +51,61 @@ impl Component for AffinitiesTable {
     }
 
     fn view(&self) -> Html {
-        view_affinities_table(&self.props.affinities)
+        html! {
+            <>
+            { self.view_filters() }
+            { self.view_affinities_table() }
+            </>
+        }
     }
 }
 
-fn view_affinities_table(affinities: &[UserAffinity]) -> Html {
-    let affinities = affinities.iter().rev().map(|a| view_affinity(a));
-    html! {
-    <ul class="devand-user-affinities">
-    { for affinities}
-    </ul>
+impl AffinitiesTable {
+    fn view_filters(&self) -> Html {
+        let selected_language = self.state.filter_language;
+
+        let on_change = self.link.callback(move |cd: ChangeData| {
+            if let ChangeData::Select(se) = cd {
+                Msg::FilterByLanguage(se.value())
+            } else {
+                Msg::Nope
+            }
+        });
+
+        let options = Language::iter()
+            .map(|lang| (lang, selected_language == Some(lang)))
+            .map(|(lang, selected)| {
+                html! { <option value=lang selected=selected>{ lang }</option> }
+            });
+
+        html! {
+        <select name="add_language" onchange=on_change>
+            <option value="" selected=(selected_language.is_none())></option>
+            { for options }
+        </select>
+        }
+    }
+
+    fn view_affinities_table(&self) -> Html {
+        let affinities = self
+            .props
+            .affinities
+            .iter()
+            .rev()
+            .filter(|x| {
+                dbg!(&self.state.filter_language);
+                if let Some(filter_language) = self.state.filter_language {
+                    x.user.languages.contains_key(&filter_language)
+                } else {
+                    true
+                }
+            })
+            .map(|a| view_affinity(a));
+        html! {
+        <ul class="devand-user-affinities">
+        { for affinities}
+        </ul>
+        }
     }
 }
 
